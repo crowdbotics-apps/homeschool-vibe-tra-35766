2518671120f89ed4ae8e45af4533e3338fb492ca
Crowdbotics <team@crowdbotics.com>
2022-06-16 23:53:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "homeschool_vibe_tra_35766",
+  url: "https://homeschool_vibe_tra_35766.botics.co",
+  api: "https://homeschool_vibe_tra_35766.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
